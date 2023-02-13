c31df49229f055cd94a466b5854dcf6ff7de7383
Nathaphong <natthaphong_p@cmu.ac.th>
2023-02-13 20:17:38 +0700

    initial

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
+import Editor from "components/Editor";
+import { useRef } from "react";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+	return <Editor />;
+}
