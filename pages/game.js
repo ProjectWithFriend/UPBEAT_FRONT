0207fd96efd52700015487866ef455eb743ccd7c
Mhingzzz <90751090+Mhingzzz@users.noreply.github.com>
2023-02-15 20:42:46 +0700

    install depencies

--- pages/game.js
+++ pages/game.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Editor from "../components/Editor";
 import HexGridDemo from "../components/Grid";
 export default function game() {
 	return (
 		<div>
 			<HexGridDemo />
+			<Editor></Editor>
 		</div>
 	);
 }
