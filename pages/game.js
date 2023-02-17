32893f8c760204d52765e06ac71717c051e01510
AuJung <natthaphong_th@cmu.ac.th>
2023-02-17 20:28:14 +0700

    - Test random city center with api (passed)

--- pages/game.js
+++ pages/game.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import Editor from "../components/Editor";
 import HexGridDemo from "../components/Grid";
+import axios from "axios";
 export default function game() {
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	let [showGrid, setShowGrid] = useState(false);
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	let [ct, setCT] = useState([]);
+	const Grid = async () => {
+		let res = await axios.get("http://localhost:8080/api/random");
+		let data = res.data;
+		for(let i in data){
+			ct.push(data[i]);
+		}
+		setShowGrid(true);
+	}
 	return (
 		<div>
-			<HexGridDemo />
+			{showGrid ? <HexGridDemo CT={ct}/> : null}
+			{/*<HexGridDemo CTP1={ctp1} CTP2={ctp2}/>*/}
 			{/* <Editor></Editor> */}
+			<button className="btn btn-primary" onClick={() => Grid()}>test</button>
 		</div>
 	);
 }
